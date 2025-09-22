ec8c12f9764c1eec35978c70d21da589634de31a
Hasan Sevinç <Hasan.Sevinc@hotmail.com.tr>
2025-09-22 23:24:17 +0300

    Add project files.

---
+++ Course1/Program.cs
@@ -0,0 +1,48 @@
+﻿
+//Console.WriteLine("Hasan SEVİNÇ");
+
+//Console.Write("Yazılım Geliştirme Uzmanı");
+
+//Console.WriteLine("Hasan SEVİNÇ\nYazılım Geliştirme Uzmanı");
+
+////todo Remove the duplicate line
+//Console.WriteLine("Hasan SEVİNÇ\tYazılım Geliştirme Uzmanı");
+
+#region Değişken İsimlendirme Kuralları
+
+// bir değişkene isim verirken değişken ismi sayı ile başlayamaz.
+
+string isim1 = "Hasan";
+string isim ="Hasan";
+Console.WriteLine(isim1);
+
+// değişken isimlendirirken boşluk bırakamazsınız
+
+//string isimsoyisim = "Hasan SEVİNÇ";
+
+//// değişken isimlendirirken özel karakter kullanamazsınız "_" operatörü hariç
+//string isim_soyisim = "Hasan SEVİNÇ";
+////string isimsoyisim = "Hasan SEVİNÇ";
+
+//string isimSoyisim = "Hasan SEVİNÇ"; // CamelCase
+//string IsimSoyisim = "Hasan SEVİNÇ"; // PascalCase
+
+//string isim_soyisim = "Hasan SEVİNÇ"; // SnakeCase
+#endregion
+
+#region String Değişkenler
+//string? Name;
+//Name = "Hasan SEVİNÇ";
+
+//Console.WriteLine(Name);
+
+string Surname = "SEVİNÇ";
+Console.WriteLine(Surname);
+#endregion
+
+
+
+
+
+
+
