41a407bf6071d38dd9106d338690decf099c8364
Hasan Sevinç <Hasan.Sevinc@hotmail.com.tr>
2025-09-25 22:29:38 +0300

    değişkenlerin devamı

--- Course1/Program.cs
+++ Course1/Program.cs
@@ -40,8 +40,6 @@ string Surname = "SEVİNÇ";
 Console.WriteLine(Surname);
 #endregion
 
-#region Integer Değişkenler
-#endregion
 
 
 
---
+++ Course2/Program.cs
@@ -0,0 +1,78 @@
+﻿#region Integer Değişkenler
+
+//int number1 = 10 , number2 = 20, number3 = 30;
+
+//int total = number1 + number2 + number3;
+//Console.WriteLine(total);
+
+//int ortalama =  (number1 + number2 + number3) / 3;
+//int kafadansayi = number1 * number2 * number3;
+
+//Console.WriteLine(ortalama);
+//Console.WriteLine(kafadansayi);
+
+//Console.WriteLine(number);
+
+#endregion
+
+#region kullanıcıdan veri alma
+
+//int number1, number2, number3;
+
+//Console.Write("1. sayıyı giriniz: ");
+//number1 = int.Parse(Console.ReadLine());
+//Console.WriteLine();
+
+//Console.Write("2. sayıyı giriniz: ");
+//number2 = Convert.ToInt16(Console.ReadLine());
+//Console.WriteLine();
+
+//Console.Write("3. sayıyı giriniz: ");
+//number3 = Convert.ToInt32(Console.ReadLine());
+
+//int Ortalama = (number1 + number2 + number3) / 3;
+//Console.WriteLine("Ortalama: " + Ortalama);
+
+//int total = number1 + number2 + number3;
+//Console.WriteLine("Toplam: " + total);
+#endregion
+
+#region Ondalıklı Sayılar (float, double, decimal)  
+
+//double doblenumber1 , doblenumber2 , doblenumber3;
+//Console.WriteLine("double number 1 : " +  doblenumber1);
+//Console.WriteLine($"double number 2 : {doblenumber2}");
+//Console.WriteLine($"double number 3 :{doblenumber3}");
+//Console.Write("1. sayıyı giriniz: ");
+//doblenumber1 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine();
+
+//Console.Write("2. sayıyı giriniz: ");
+//doblenumber2 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine();
+
+//Console.Write("3. sayıyı giriniz: ");
+//doblenumber3 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine();
+
+//double Ortalama = (doblenumber1 + doblenumber2 + doblenumber3) / 3;
+//Console.WriteLine("Ortalama: " + Ortalama);
+//double total = doblenumber1 + doblenumber2 + doblenumber3;
+//Console.WriteLine("Toplam: " + total);
+#endregion
+
+#region Tarihsel veri tipleri 
+
+//DateTime date = DateTime.Now;
+
+//Console.WriteLine($"Tam Tarih =>  {date}");
+//Console.WriteLine($"Yıl =>  {date.Year}");
+//Console.WriteLine($"Dakika =>  {date.Minute}");
+//Console.WriteLine($"Yılın Günü =>  {date.DayOfYear}");
+
+//DateTimeOffset dateTimeOffset = DateTimeOffset.Now;
+//Console.WriteLine($"Tam Tarih =>  {dateTimeOffset}");
+
+#endregion
+
+
